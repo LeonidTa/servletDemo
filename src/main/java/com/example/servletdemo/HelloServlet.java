1ebc761eb28570865ceba5fb37f5411647bf5cdf
LeonidTa <leonid.tahiraj@tutanota.com>
2021-12-12 14:26:31 +0100

    created a few parameters in the web.xml file and used them in the TestParamServlet

--- src/main/java/com/example/servletdemo/HelloServlet.java
+++ src/main/java/com/example/servletdemo/HelloServlet.java
@@ -6,22 +6,27 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //Step 1: set the content type
         response.setContentType("text/html");
 
-        // Hello
+        //Step 2: get the printwriter
         PrintWriter out = response.getWriter();
+
+        //Step 3: generate HTML content
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+
+        out.println("<h1>Hello World</h1>");
+        out.println("<hr>");
+        out.println("Time on the server is: " + new java.util.Date());
+
+
         out.println("</body></html>");
     }
 
-    public void destroy() {
-    }
+
+
+
 }
